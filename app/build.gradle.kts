55c6696fd75d6dbd18376704df65539b0dec5ff1
Javier Alejandro Puras Lombillo <javijandro99@gmail.com>
2024-02-28 12:10:16 +0100

    Paso 5.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -79,4 +79,7 @@ dependencies {
     
     debugImplementation("androidx.compose.ui:ui-test-manifest")
     debugImplementation("androidx.compose.ui:ui-tooling")
+
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-scalars:2.9.0")
 }
