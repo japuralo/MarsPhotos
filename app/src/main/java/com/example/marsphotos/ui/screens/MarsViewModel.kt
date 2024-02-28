75ccfcdd24b0807a5cb871743b2ef24bcad35a2b
Javier Alejandro Puras Lombillo <javijandro99@gmail.com>
2024-02-28 12:55:02 +0100

    Paso 5.7

--- app/src/main/java/com/example/marsphotos/ui/screens/MarsViewModel.kt
+++ app/src/main/java/com/example/marsphotos/ui/screens/MarsViewModel.kt
@@ -22,10 +22,18 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.marsphotos.network.MarsApi
 import kotlinx.coroutines.launch
+import java.io.IOException
+
+sealed interface MarsUiState
+{
+    data class Success(val photos: String): MarsUiState
+    object Error: MarsUiState
+    object Loading: MarsUiState
+}
 
 class MarsViewModel : ViewModel() {
     /** The mutable State that stores the status of the most recent request */
-    var marsUiState: String by mutableStateOf("")
+    var marsUiState: MarsUiState by mutableStateOf(MarsUiState.Loading)
         private set
 
     /**
@@ -39,11 +47,18 @@ class MarsViewModel : ViewModel() {
      * Gets Mars photos information from the Mars API Retrofit service and updates the
      * [MarsPhoto] [List] [MutableList].
      */
-    fun getMarsPhotos()
+    private fun getMarsPhotos()
     {
         viewModelScope.launch {
-            val listResult = MarsApi.retrofitService.getPhotos()
-            marsUiState = listResult
+            try
+            {
+                val listResult = MarsApi.retrofitService.getPhotos()
+                marsUiState = MarsUiState.Success(listResult)
+            }
+            catch (e: IOException)
+            {
+                MarsUiState.Error
+            }
         }
     }
 }
