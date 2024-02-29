b5bd9dab4b07352b2282523374c8a8f7c5330e21
Javier Alejandro Puras Lombillo <javijandro99@gmail.com>
2024-02-29 12:45:46 +0100

    Paso 2.10

---
+++ app/src/test/java/com/example/marsphotos/fake/FakeNetworkMarsRepository.kt
@@ -0,0 +1,12 @@
+package com.example.marsphotos.fake
+
+import com.example.marsphotos.data.MarsPhotosRepository
+import com.example.marsphotos.network.MarsPhoto
+
+class FakeNetworkMarsRepository: MarsPhotosRepository
+{
+    override suspend fun getMarsPhotos(): List<MarsPhoto>
+    {
+        return FakeDataSource.photosList
+    }
+}
